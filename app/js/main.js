c3f74d8ed66078e880adda3b8bb02b5563170254
Leonid <leonardoShubin@gmail.com>
2019-05-26 21:15:04 +0300

    for review

--- app/js/main.js
+++ app/js/main.js
@@ -1,63 +1,78 @@
 const offerSwiper = new Swiper(".special-offer .swiper-container", {
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+const whyUsSwiper = new Swiper(".why-us .swiper-container", {
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+const teamSwiper = new Swiper(".our-team .swiper-container", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 35,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 30
     }
-  });
-  
-  const whyUsSwiper = new Swiper(".why-us .swiper-container", {
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
-  
-  const teamSwiper = new Swiper('.our-team .swiper-container', {
-        loop: true,
-        slidesPerView: 4,
-        spaceBetween: 35,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
 
 const appModal = {
-    el: null,
-    buttons: [],
-    init(opts) {
-        this.el = document.querySelector(opts.el);
-        this.buttons = document.querySelectorAll(opts.buttons);
+  el: null,
+  buttons: [],
+  init(opts) {
+    this.el = document.querySelector(opts.el);
+    this.buttons = document.querySelectorAll(opts.buttons);
 
-        this.buttons.forEach(btn => {
-            btn.addEventListener("click", () => this.open());
-        });
+    this.buttons.forEach(btn => {
+      btn.addEventListener("click", () => this.open());
+    });
 
-        document.addEventListener("keydown", e => {
-            if (e.keyCode === 27) this.close();
-        });
+    document.addEventListener("keydown", e => {
+      if (e.keyCode === 27) this.close();
+    });
 
-        this.el.addEventListener("click", e => {
-            if (e.target.classList.contains("modal")) this.close();
-        });
-    },
-    open() {
-        document.body.style.overflow = "hidden";
+    this.el.addEventListener("click", e => {
+      if (e.target.classList.contains("modal")) this.close();
+    });
+  },
+  open() {
+    document.body.style.overflow = "hidden";
 
-        this.el.classList.add("modal--fade-in");
-        this.el.classList.remove("modal--fade-out");
-    },
-    close() {
-        document.body.style.overflow = '';
-        
-        this.el.classList.add("modal--fade-out");
-        this.el.classList.remove("modal--fade-in");
-    }
-    };
+    this.el.classList.add("modal--fade-in");
+    this.el.classList.remove("modal--fade-out");
+  },
+  close() {
+    document.body.style.overflow = "";
+
+    this.el.classList.add("modal--fade-out");
+    this.el.classList.remove("modal--fade-in");
+  }
+};
 
 appModal.init({
-    el: ".modal",
-    buttons: ".button--modal"
+  el: ".modal",
+  buttons: ".button--modal"
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,6 @@ gulp.task('useref', function() {
 
   return gulp.src('app/*.html')
     .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
 });
